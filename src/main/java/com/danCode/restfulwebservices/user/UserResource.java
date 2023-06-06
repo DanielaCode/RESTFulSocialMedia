85198a10303d9861808d803e20d1099379da8c00
Daniela <danielagp96@gmail.com>
2023-06-05 18:02:06 -0600

    -currently if I create an object without name or with a birthday on the future it is possible so ir is solved here -adding spring-boot-starter-validation -@Valid using this annotation to validate the input parameters in the controller taking into account what annotation the object has

--- src/main/java/com/danCode/restfulwebservices/user/UserResource.java
+++ src/main/java/com/danCode/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.danCode.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -32,7 +33,7 @@ public class UserResource {
     }
     //creating new user
     @PostMapping("/users")
-    public ResponseEntity<User> save(@RequestBody User user){
+    public ResponseEntity<User> save(@Valid @RequestBody User user){
         User savedUser= service.save(user);
         URI location= ServletUriComponentsBuilder
                         .fromCurrentRequest()
